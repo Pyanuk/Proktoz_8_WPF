62fe0cd6c81020f26e98e8e1a39b766de85593bc
Pyanuk <rhalikov735@gmail.com>
2024-06-07 15:46:12 +0300

    pr8

---
+++ Libraries/App.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+
+namespace Libraries
+{
+    public partial class App : Application
+    {
+        private static string theme;
+        private static string language;
+
+        public static string Theme
+        {
+            get { return theme; }
+            set
+            {
+                theme = value;
+                var dict = new ResourceDictionary { Source = new Uri("/LibraryThemes;component/Themes/" + value + ".xaml", UriKind.Relative) };
+
+                if (Current.Resources.MergedDictionaries.Count > 0)
+                {
+                    Current.Resources.MergedDictionaries.RemoveAt(0);
+                }
+                Current.Resources.MergedDictionaries.Insert(0, dict);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllText($"{desktop}\\theme.txt", value);
+            }
+        }
+
+        public static string Language
+        {
+            get { return language; }
+            set
+            {
+                language = value;
+                var t = new ResourceDictionary { Source = new Uri("/RussianEnglish;component/Language/" + value + ".xaml", UriKind.Relative) };
+
+                if (Current.Resources.MergedDictionaries.Count > 1)
+                {
+                    Current.Resources.MergedDictionaries.RemoveAt(2);
+                }
+                Current.Resources.MergedDictionaries.Insert(2, t);
+
+                var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                File.WriteAllText($"{desktop}\\language.txt", value);
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            var desktop = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            if (File.Exists($"{desktop}\\theme.txt"))
+            {
+                Theme = File.ReadAllText($"{desktop}\\theme.txt");
+            }
+            if (File.Exists($"{desktop}\\language.txt"))
+            {
+                Language = File.ReadAllText($"{desktop}\\language.txt");
+            }
+        }
+    }
+}
---
+++ Libraries/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System.Windows;
+using LibraryJson;
+using Path = System.IO.Path;
+
+namespace Libraries
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void indigoTheme_Click(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "Purple";
+        }
+
+        private void aquamarineTheme_Click(object sender, RoutedEventArgs e)
+        {
+            App.Theme = "White";
+        }
+
+        private void serializationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string textToSerialize = inputText.Text;
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string jsonFilePath = Path.Combine(desktopPath, "Ser.json");
+            Json.SerializeToFile(textToSerialize, jsonFilePath);
+
+            inputText.Text = "";
+        }
+
+        private void deserializationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string jsonFilePath = Path.Combine(desktopPath, "Ser.json");
+            string deserializedText = Json.DeserializeFromFile<string>(jsonFilePath);
+            if (!string.IsNullOrEmpty(deserializedText))
+            {
+                dataLbx.Items.Add(deserializedText);
+            }
+        }
+
+        private void english_Click(object sender, RoutedEventArgs e)
+        {
+            App.Language = "Russian";
+        }
+
+        private void russian_Click(object sender, RoutedEventArgs e)
+        {
+            App.Language = "English";
+        }
+    }
+}
